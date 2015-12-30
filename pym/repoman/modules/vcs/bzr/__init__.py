398444bfec9dea8ee37ea423dddaf73a0d535d6f
Brian Dolbec <dolsen@gentoo.org>
2015-12-30 15:24:22 -0800

    repoman: New vcs plugin system
    Migrate Changes to the vcs plugin system
    Move ChangesBase to the vcs pkg.
    Establish the plugin system at repoman/modules/vcs.
    Sub-modules will be directly in the modules/vcs module.
    Common files used by the submodules are in the vcs directory.
    Create a None named vcs module to handle all non-vcs type repositories.
    Signed-off-by: Brian Dolbec <dolsen@gentoo.org>

---
+++ pym/repoman/modules/vcs/bzr/__init__.py
@@ -0,0 +1,31 @@
+# Copyright 2014-2015 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+doc = """BZR plug-in module for portage.
+Performs variaous Bazaar actions and checks on repositories."""
+__doc__ = doc[:]
+
+
+module_spec = {
+	'name': 'bzr',
+	'description': doc,
+	'provides':{
+		'bzr-module': {
+			'name': "bzrstatus",
+			'class': "Status",
+			'description': doc,
+			'functions': ['check', 'supports_gpg_sign', 'detect_conflicts'],
+			'func_desc': {
+			},
+			'vcs_preserves_mtime': True,
+		},
+		'bzr-changes': {
+			'name': "bzrchanges",
+			'class': "Changes",
+			'description': doc,
+			'functions': ['scan'],
+			'func_desc': {
+			},
+		},
+	}
+}
