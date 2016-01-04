40c2bdf3d00f05d9bcbf74796c460e909eea4742
Brian Dolbec <dolsen@gentoo.org>
2016-01-03 23:55:55 -0800

    repoman: Create a new Options class plugin
    This handles an options.force bypass using the is_forced() from withing the plugin system.

---
+++ pym/repoman/modules/scan/options/__init__.py
@@ -0,0 +1,23 @@
+# Copyright 2015-2016 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+doc = """Options plug-in module for repoman.
+Performs option related actions on ebuilds."""
+__doc__ = doc[:]
+
+
+module_spec = {
+	'name': 'options',
+	'description': doc,
+	'provides':{
+		'options-module': {
+			'name': "options",
+			'class': "Options",
+			'description': doc,
+			'functions': ['is_forced'],
+			'func_desc': {
+			},
+		},
+	}
+}
+
